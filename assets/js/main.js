1829cc5c6782254ecdcba0beb838004c10f745f5
younessidbakkasse <std.youness@gmail.com>
2021-02-06 20:39:51 +0100

    Added the dbl click animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,3 +143,51 @@ main.addEventListener('click', () => {
 
 
 // Working with windows
+const trashIcon = document.querySelector('.icon.trash');
+const trashWin = document.querySelector('.window.trash');
+const closeTrash = document.querySelector('.window.trash .window-close_btn');
+
+trashIcon.ondblclick = (e) => {
+    if(trashWin.style.visibility === "visible") {
+        trashWin.classList.add('anim-bounce');
+        setTimeout(function(){
+            trashWin.classList.remove('anim-bounce');
+        },2000);
+    } else {
+        trashWin.style.visibility = "visible";
+    }
+};
+
+closeTrash.onmousedown = (e) => {
+    console.log('closed');
+    trashWin.style.visibility = "hidden";      
+};
+
+
+(() => {
+    let icon = document.querySelector('.window');
+    let pos1 = 0, pos2 = 0;
+    icon.onmousedown = (e) => {
+        icon.focus();
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = (e) => {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        };
+        document.onmousemove = (e) => {
+            e = e || window.event;
+        e.preventDefault();
+
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        icon.style.top = icon.offsetTop < 0 ? "0" : `${icon.offsetTop - pos2}px`;
+        icon.style.left = `${icon.offsetLeft - pos1}px`;
+        };
+    };
+})();
